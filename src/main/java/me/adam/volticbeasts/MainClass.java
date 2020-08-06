ea8dfda5b60eb6f94b616e97f4fe270524616440
Amestri-s <btamestris@gmail.com>
2020-08-06 21:47:29 +0100

    Initial commit

---
+++ src/main/java/me/adam/volticbeasts/MainClass.java
@@ -0,0 +1,9 @@
+package me.adam.volticbeasts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainClass extends JavaPlugin {
+    public void onEnable(){
+        getLogger().info("Working");
+    }
+}
